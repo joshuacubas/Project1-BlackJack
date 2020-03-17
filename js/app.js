6b10bc0e857f77bf9124829f8fa5b459caf9953f
Joshua M Cubas <joshuacubas@yahoo.com>
2020-03-17 14:13:24 -0500

    added start,placeBets,randomCardIdx,addRandomCardToHand functions to game app

--- js/app.js
+++ js/app.js
@@ -1,1 +1,84 @@
 console.log("Lets Play BlackJack!!")
+
+
+//make a constructor for Cards
+class Card {
+	constructor(suit,numFace){
+		this.suit = suit
+		this.numFace = numFace
+		this.name = `${numFace} of ${suit}`
+	}
+}
+
+
+const game = {
+
+	player1Hand : [],
+
+	player1Bank : 10,
+
+	player1Bet : 0,
+
+	player2Hand : [],
+
+	player2Bank: 10,
+
+	player2Bet : 0,
+
+	dealerHand : [],
+
+	dealerBank : 20,
+
+	betMultiplier : 2,
+
+	deck : [],
+
+	discardPile : [],
+
+	generateDeck : () => {
+		console.log('generating')
+		
+		let numFaces = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
+		let suits = ['CLUBS',"SPADES",'HEARTS','DIAMONDS']
+		
+		
+		for(let i = 0; i < suits.length; i++){
+			
+			let curSuit = suits[i]
+		
+			for(j = 0; j < numFaces.length; j++){
+
+				let curNumFace = numFaces[j]
+				game.deck.push(new Card(curSuit,curNumFace))
+			}
+		}
+	}, 
+
+	start : ()=>{
+		game.generateDeck()
+		let dealToArray = [game.player1Hand,game.player1Hand,game.player2Hand,game.player2Hand,game.dealerHand]
+		game.addRandomCardToHand(game.player1Hand)
+	},
+
+	placeBets : () => {},
+
+	randomCardIdx : () => {
+		let randomIndex = Math.floor( Math.random()*game.deck.length );
+		let randomCard = game.deck[randomIndex]
+
+		return randomIndex
+	}, 
+
+	addRandomCardToHand : (whoseHand) => {
+
+		let index = game.randomCardIdx()
+		let card = game.deck.splice(index,1)
+		whoseHand.push(card)
+	}, 
+
+
+
+}
+
+
+//Listeners below
